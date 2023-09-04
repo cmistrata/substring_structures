f3ac7a91f729e2f1f618e23036d79eebef0ea93a
cmistrata <cmistrata@gmail.com>
2023-09-03 23:02:23 -0700

    Add documentation and use set instead of list.

--- src/aho_corasick.py
+++ src/aho_corasick.py
@@ -1,40 +1,53 @@
-class ACNode:
-    creation_time_counter = 0
+from typing import Collection
 
+
+class ACNode:
     def __init__(self, value: str | None = None):
         self.value = value
         self.children: dict[str, ACNode] = {}
         self.suffix_link: ACNode | None = None
-        self._creation_time = ACNode.creation_time_counter
-        ACNode.creation_time_counter += 1
-
-    def __hash__(self) -> int:
-        return self._creation_time
 
 
 class AhoCorasick:
-    def __init__(self, strings: list[str]):
+    """Structure used to check a known set of strings is contained in an arbitrary superstring.
+
+    Based on the Aho-Corasick algorithm (https://en.wikipedia.org/wiki/Aho%E2%80%93Corasick_algorithm),
+    this structure can be computed in O(N) time for a set of strings "SubstringsSet" of total length N. It can
+    then be used to efficiently check which strings in "SubstringsSet" are in a larger string "superstring"
+    using the `find_substrings_in_superstring(superstring)` method.
+    """
+
+    def __init__(self, strings: Collection[str]):
+        if isinstance(strings, str):
+            raise ValueError(
+                f"'`strings` should be a collection of strings but was passed a single string '{strings}'."
+            )
         self.root = ACNode()
-        current_nodes_by_string_index = [self.root] * len(strings)
+        if "" in strings:
+            self.root.value = ""
+
+        current_nodes_by_string = {string: self.root for string in strings}
 
         current_char_index = 0
-        while any(current_char_index < len(string) for string in strings):
-            for string_index, string in enumerate(strings):
+        while any(current_nodes_by_string):
+            strings_shorter_than_current_char_index = []
+            for string, current_node in current_nodes_by_string.items():
                 if current_char_index >= len(string):
+                    strings_shorter_than_current_char_index.append(string)
                     continue
+
                 current_char = string[current_char_index]
-                current_node_of_string = current_nodes_by_string_index[string_index]
 
                 # Add a new child node if it does not already exist.
-                if current_char not in current_node_of_string.children:
+                if current_char not in current_node.children:
                     child_node = ACNode()
-                    current_node_of_string.children[current_char] = child_node
+                    current_node.children[current_char] = child_node
                     child_node.suffix_link = self._move_forward_from_node(
-                        node=current_node_of_string.suffix_link,
+                        node=current_node.suffix_link,
                         char=current_char,
                     )
                 else:
-                    child_node = current_node_of_string.children[current_char]
+                    child_node = current_node.children[current_char]
 
                 # Set the value of the child node to the current string if
                 # we have reached the end of the string.
@@ -42,8 +55,12 @@ class AhoCorasick:
                 if at_last_char_of_string:
                     child_node.value = string
 
-                current_nodes_by_string_index[string_index] = child_node
+                current_nodes_by_string[string] = child_node
 
+            for (
+                string_shorter_than_current_char_index
+            ) in strings_shorter_than_current_char_index:
+                current_nodes_by_string.pop(string_shorter_than_current_char_index)
             current_char_index += 1
 
     def _move_forward_from_node(self, node: ACNode | None, char: str):
@@ -56,10 +73,12 @@ class AhoCorasick:
         # suffix link of the root node.
         return self.root
 
-    def list_substrings_in_superstring(self, superstring: str):
+    def find_substrings_in_superstring(self, superstring: str):
         found_substrings = set()
-        current_nodes = {self.root}
+        if self.root.value is not None:
+            found_substrings.add("")
 
+        current_nodes = {self.root}
         for char in superstring:
             next_nodes = {self.root}
             for node in current_nodes:
